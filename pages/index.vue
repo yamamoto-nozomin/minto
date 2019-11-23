c07e9406daf752c4b9f19b18727e7d1557fb24e5
yamamoto nozomi <yamamoto.nozomi.w@gmail.com>
2019-11-23 14:08:20 +0900

    default branchをdevelopにしてみた
    コミットテスト

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <logo/>
-      <h1 class="title">nuxtjsaksadjfaisfja</h1>
+      <h1 class="title">nuxt</h1>
       <h2 class="subtitle">My astonishing Nuxt.js project</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
