f354a5fce8f5d58f0394d3ec40ff4074f1e7ea30
yamamoto nozomi <yamamoto.nozomi.w@gmail.com>
2019-11-23 14:01:10 +0900

    コミットテスト2
    gitと連携できるか

---
+++ pages/minto_main.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <div>
+      <logo/>
+      <h1 class="title">nuxtjs</h1>
+      <h2 class="subtitle">My astonishing Nuxt.js project</h2>
+    </div>
+  </div>
+</template>
