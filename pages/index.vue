6ac2291a9edc45a1f8955041e9a3e503746dfb87
yamamoto nozomi <yamamoto.nozomi.w@gmail.com>
2019-11-23 14:05:39 +0900

    develop　branch作ったけど
    master？develop？

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <logo/>
-      <h1 class="title">nuxtjs</h1>
+      <h1 class="title">nuxtjsaksadjfaisfja</h1>
       <h2 class="subtitle">My astonishing Nuxt.js project</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
