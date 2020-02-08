7d08cc74c3c7b691abe6bed5d8d8fb3eec8b2881
yamamoto nozomi <yamamoto.nozomi.w@gmail.com>
2020-02-08 11:53:25 +0900

    ページ構成
    部品分けた

--- pages/minto_main.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container">
-    <div>
-      <logo/>
-      <h1 class="title">nuxtjs</h1>
-      <h2 class="subtitle">My astonishing Nuxt.js project</h2>
-    </div>
-  </div>
-</template>
